33207fcd1c5363330d47fbba47bfb5488c5a4d05
Illia Kozliuk <ikozluk160@gmail.com>
2023-04-01 02:31:16 +0300

    feat: add tailwind

---
+++ components/Header.tsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
+export function Header() {
+  return <header>1</header>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
+// eslint-disable-next-line import/no-unresolved
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component }: AppProps) {
-  return <Component />;
+export default function App({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
 }
